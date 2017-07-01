3334f5fef895e7b95a1177f28a58344203c690aa
SHADE <manuel.kleber30@gmail.com>
2017-07-01 23:51:40 +0200

    FXServer
    RalFinger Testserver: Stand Manuel

---
+++ FXServer/resources/[test]/keks/__resource.lua
@@ -0,0 +1,11 @@
+files {
+    'index.html',
+    'keks.css',
+    'loadscreen.jpg',
+	'bankgothic.ttf',
+	'loadscreen.wav'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
